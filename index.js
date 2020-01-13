158c4433dfe9e2790456977d78618f67389a7e5a
seanpgallivan <seanpgallivan@hotmail.com>
2020-01-12 22:07:13 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = array => {
+  let win = array.find(season => season.result == "W")
+  if (win) {
+    return win.year
+  }
+}
